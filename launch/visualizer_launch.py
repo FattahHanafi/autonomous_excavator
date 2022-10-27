7730ec796bf81b135ba3fb5e3b25ecd92c66ff62
FattahHanafi <fattah.hanafisheikhha@ontariotechu.net>
2022-10-27 12:54:25 -0400

    Fixed rviz config file loading

--- launch/visualizer_launch.py
+++ launch/visualizer_launch.py
@@ -4,8 +4,11 @@ from launch_ros.actions import Node
 import os.path
 
 urdf_file_name = 'electrical_excavator.urdf'
+rviz_file_name = 'machine_visualizer.rviz'
+
 urdf = os.path.join(get_package_share_directory('autonomous_excavator'), 'urdf', urdf_file_name)
-print(urdf)
+rviz = os.path.join(get_package_share_directory('autonomous_excavator'), 'config', rviz_file_name)
+
 with open(urdf, 'r') as infp:
     robot_desc = infp.read()
 
@@ -13,25 +16,18 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='autonomous_excavator',
-            executable='joint_publisher',
-            ),
-        #  Node(
-            #  package='autonomous_excavator',
-            #  executable='excavator_tf2_broadcaster',
-        #  ),
+            executable='joint_publisher'),
         Node(
             package='autonomous_excavator',
-            executable='blade_node',
-        ),
+            executable='blade_node'),
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            output='screen',
             parameters=[{'robot_description': robot_desc}],
             arguments=[urdf]),
-        #  Node(
-            #  package='rviz2',
-            #  executable='rviz2',
-            #  arguments=['-d', [os.path.join(get_package_share_directory('autonomous_excavator'), 'config', 'machine_visualizer.rviz')]]
-            #  ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen',
+            arguments=['-d', rviz]),
     ])
