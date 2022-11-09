f235bc2616692321c36726f85bb5aef3e1afea6e
FattahHanafi <fattah.hanafisheikhha@ontariotechu.net>
2022-11-09 18:15:00 -0500

    Implemented all actuators and sensors

--- Arduino/ElectricalExcavator/ElectricalExcavator.ino
+++ Arduino/ElectricalExcavator/ElectricalExcavator.ino
@@ -9,24 +9,25 @@ double target[NUM_ACT];
 double actual[NUM_ACT];
 double voltage[NUM_ACT];
 
-#define KP 3
-#define KI 0.3
-#define KD 3
+#define KP 3.5d
+#define KI 0.0d
+#define KD 5.5d
+#define MIN_VOLTAGE FROM_LOW + 10
 
-#define OUTPUT_MIN -150
-#define OUTPUT_MAX 150
+#define OUTPUT_MIN -150.0d
+#define OUTPUT_MAX 150.0d
 
-#define FROM_LOW 25    // Minimum value of sensor
-#define FROM_HIGH 882  // Maximum value of sensor
-#define TO_LOW 408     // Stroke related to minimum value of sensor
-#define TO_HIGH 600    // Stroke related to maximum value of sensor
+#define FROM_LOW 25.0d    // Minimum value of sensor
+#define FROM_HIGH 882.0d  // Maximum value of sensor
+#define TO_LOW 408.0d     // Stroke related to minimum value of sensor
+#define TO_HIGH 600.0d    // Stroke related to maximum value of sensor
 
 AutoPID PID_1(&actual[0], &target[0], &voltage[0], OUTPUT_MIN, OUTPUT_MAX, KP, KI, KD);
 AutoPID PID_2(&actual[1], &target[1], &voltage[1], OUTPUT_MIN, OUTPUT_MAX, KP, KI, KD);
 AutoPID PID_3(&actual[2], &target[2], &voltage[2], OUTPUT_MIN, OUTPUT_MAX, KP, KI, KD);
 AutoPID PID_4(&actual[3], &target[3], &voltage[3], OUTPUT_MIN, OUTPUT_MAX, KP, KI, KD);
 
-const int pwm_pins[] = { 13, 10, 11, 12};
+const int pwm_pins[] = { 13, 12, 11, 10 };
 const int sen_pins[] = { A0, A1, A2, A3 };
 
 void setup() {
@@ -55,12 +56,13 @@ void setup() {
 }
 
 void checkSerial() {
-  if (Serial.available() < sizeof(target)) {
+
+  if (Serial.available() < 6)  {
     return;
   }
 
   for (uint8_t i = 0; i < NUM_ACT; ++i) {
-    target[i] = Serial.parseFloat();
+    target[i] = double(Serial.parseFloat());
   }
 }
 
@@ -84,15 +86,22 @@ void loop() {
   direction = 0;
   for (uint8_t i = 0; i < NUM_ACT; ++i) {
     Serial.print(actual[i]);
-    Serial.print(' ');
-    direction |= (voltage[i] > 0) << i;
+    Serial.print(" ");
+    // Serial.print(target[i]);
+    // Serial.print(" ... ");
+
+    if (voltage[i] >= 0) {
+      direction |= (0b01) << (2 * i);
+    }
+    else {
+      direction |= (0b10) << (2 * i);
+    }
   }
   Serial.print('\n');
-  direction |= (~direction) << NUM_ACT;
 
   DIR_PORT = direction;
   for (uint8_t i = 0; i < NUM_ACT; i++) {
-    analogWrite(pwm_pins[i], int(map(abs(voltage[i]), TO_LOW, TO_HIGH, FROM_LOW, FROM_HIGH)));
+    analogWrite(pwm_pins[i], (abs(voltage[i]) > MIN_VOLTAGE) ?  int(map(abs(voltage[i]), TO_LOW, TO_HIGH, FROM_LOW, FROM_HIGH)) : 0);
   }
   delay(1);
 }
